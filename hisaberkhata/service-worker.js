9234543eb09d7ca042327397fdbc78e74871530b
Antar Saha <109027878+antarweb@users.noreply.github.com>
2025-03-19 01:49:07 +0600

    Update service-worker.js

--- hisaberkhata/service-worker.js
+++ hisaberkhata/service-worker.js
@@ -1,27 +1,36 @@
-const CACHE_NAME = "hisaberkhata-cache-v1";
-const urlsToCache = [
-    "/hisaberkhata/",
-    "/hisaberkhata/index.html",
-    "/hisaberkhata/style.css",
-    "/hisaberkhata/script.js",
-    "/hisaberkhata/icon-192x192.png",
-    "/hisaberkhata/icon-512x512.png"
-];
-
-// Install Service Worker and Cache Files
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('hisaberkhata-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        // Add any other assets you need to cache
+      ]);
+    })
+  );
 });
 
-// Fetch Cached Files When Offline
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['hisaberkhata-cache'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
