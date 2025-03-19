3e3067f6544c012b00978422c3ac3bf96e7b7bf2
Antar Saha <109027878+antarweb@users.noreply.github.com>
2025-03-19 15:50:02 +0600

    Create sw.js

---
+++ youtube/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('pwa-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                'https://m.youtube.com'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
